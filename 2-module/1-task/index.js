1dc11494f42763e0dc1d0b0904042cf444e88c26
VasiliiKiselevich <kvg681982@gmail.com>
2022-03-24 02:09:45 +0300

    HW_2_lesson

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
-function sumSalary(salaries) {
-  // ваш код...
-}
+function sumSalary(salaries) {  
+  let sum = 0;
+  for (let key in salaries) {
+  if (isFinite(salaries[key]) ){
+    sum += salaries[key];    
+    }    
+  }
+  return sum;
+  };
